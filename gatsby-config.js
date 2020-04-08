2ff7d66751fa2b6e5adde6a5ddd86c804521c397
Duane Cilliers <hello@duane.co.za>
2020-04-08 16:32:51 +0200

    Update Gatsby config with gatsby-source-filesystem for pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,14 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
-    },
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `pages`,
+				path: `${__dirname}/src/pages`,
+			},
+		},
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
