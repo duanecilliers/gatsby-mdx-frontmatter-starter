1d03a6b473c2b32b60320d79c0d4f8d86b210f26
Duane Cilliers <hello@duane.co.za>
2020-04-08 17:43:04 +0200

    Add content/posts dir and update gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,13 @@ module.exports = {
         path: `${__dirname}/src/pages`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/content/posts`,
+      },
+    },
     'gatsby-plugin-mdx',
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
