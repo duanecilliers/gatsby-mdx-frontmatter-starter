d48dd3bfd4a8ec7f33dfa9a89c6c12cd7988cce2
Duane Cilliers <hello@duane.co.za>
2020-04-08 18:20:01 +0200

    Update post template to support items frontmatter

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -4,21 +4,37 @@ import { MDXProvider } from '@mdx-js/react'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 import Layout from '../components/layout'
-// import Image from '../components/image'
-// import SEO from '../components/seo'
+import SEO from '../components/seo'
 import Button from '../components/button'
 
 const shortcodes = { Link, Button }
 
 export default function PostTemplate({ data: { mdx } }) {
+  const { title, items } = mdx.frontmatter
+
   return (
+    <MDXProvider components={shortcodes}>
     <Layout>
-      {/* <SEO title='Home' /> */}
-      <h1>{mdx.frontmatter.title}</h1>
-      <MDXProvider components={shortcodes}>
+      <SEO title={title} />
+      <h1>{title}</h1>
+        {items && (
+          <>
+            <h2>Items from frontmatter. Each item contains MDX.</h2>
+            <ul>
+              {
+                items.map(item => (
+                  <li key={item.value}>
+                    <MDXRenderer>{item.value}</MDXRenderer>
+                  </li>
+                ))
+              }
+            </ul>
+          </>
+        )}
+
         <MDXRenderer>{mdx.body}</MDXRenderer>
-      </MDXProvider>
-    </Layout>
+      </Layout>
+    </MDXProvider>
   )
 }
 
@@ -28,7 +44,10 @@ export const pageQuery = graphql`
       id
       body
       frontmatter {
-        title
+        title,
+        items {
+          value
+        }
       }
     }
   }
